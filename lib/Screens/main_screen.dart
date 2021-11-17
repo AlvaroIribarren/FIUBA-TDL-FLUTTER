a51142a90509e7779d60727647eabb6d696f4252
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2021-11-17 19:43:53 -0300

    Addig Cards models

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Welcome/components/body.dart';
-
+import 'model/cards.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var mazo = Mazo();
+
     return Scaffold(
-      body: Text("Hola"),
+      body: Text(mazo.dealCards().toString())
     );
   }
 }
